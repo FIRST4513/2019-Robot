efd7cc9a7ad1aec97b6337fa89631157789f3a46
Clutterbutter <44621971+Clutterbutter@users.noreply.github.com>
2019-11-12 15:04:54 -0800

    Update Cargo_Eject_Cmd.java
    Changed false to true.

--- src/robot/commands/Cargo_Eject_Cmd.java
+++ src/robot/commands/Cargo_Eject_Cmd.java
@@ -48,7 +48,7 @@ public class Cargo_Eject_Cmd extends Command {
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return true;
     }
 
     // Called once after isFinished returns true
